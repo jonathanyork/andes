24fc25598691656c308610ccb7cf4e8d0f5fcce2
Jonathan York <jonathan.york@gmail.com>
2017-08-16 21:48:26 -0400

    Updated traverser

--- src/main/scala/com/bigdlittled/andes/Main.scala
+++ src/main/scala/com/bigdlittled/andes/Main.scala
@@ -5,24 +5,35 @@ import scalax.collection.Graph // or scalax.collection.mutable.Graph
 import scalax.collection.GraphPredef._, scalax.collection.GraphEdge._
 import scalax.collection.edge.LDiEdge     // labeled directed edge
 import scalax.collection.edge.Implicits._ // shortcuts
+import scala.collection.mutable.ArrayBuffer
 
 object Main extends App {
  import Holding._
  
  val g = Graph(
    Investment("Portfolio")~>Investment("Equities") ### 0.5,
-   Investment("Portfolio")~>Investment("Bonds") ### 0.5)
+     Investment("Equities")~>Investment("US Equities") ### 0.6,
+     Investment("Equities")~>Investment("EUR Equities") ### 0.4,
+   Investment("Portfolio")~>Investment("Bonds") ### 0.5,
+     Investment("Bonds")~>Investment("Nominal Bonds") ### 0.7,
+     Investment("Bonds")~>Investment("IL Bonds") ### 0.3)
       
  g.nodes.foreach(n => println("Node = '" + n + "'"))
 
  val p = g get Investment("Portfolio")
  
- show(p)
+ val (n0, n1, n2) = (g get Investment("Portfolio"), g get Investment("Equities"), g get Investment("Bonds"))
  
- def show(investment: Investment): Unit = {
-   println(p)
-   (p diSuccessors).foreach(show(_))
+ val result = (ArrayBuffer.empty[String] /: p.innerNodeDownUpTraverser) {
+   (buf, param) => param match {
+     case (down, node) => 
+       if (down) buf += (if (node eq p) "(" else "[") += node.toString
+       else      buf += (if (node eq p) ")" else "]")
+   }
  }
+ 
+ println(result)
+ 
 }
 
 case class Investment(name: String)
