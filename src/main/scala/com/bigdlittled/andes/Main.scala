9168bb7678d5ee686552f294fc3f925d49fb9a4d
Jonathan York <jonathan.york@gmail.com>
2017-08-19 01:32:33 -0400

    Added traversers

--- src/main/scala/com/bigdlittled/andes/Main.scala
+++ src/main/scala/com/bigdlittled/andes/Main.scala
@@ -1,61 +1,62 @@
 package com.bigdlittled.andes
 
 import scalaz._
-import scalax.collection.Graph // or scalax.collection.mutable.Graph
+import scalax.collection.Graph
 import scalax.collection.GraphPredef._, scalax.collection.GraphEdge._
 import scalax.collection.edge.LDiEdge     // labeled directed edge
 import scalax.collection.edge.Implicits._ // shortcuts
 import scala.collection.mutable.ArrayBuffer
+import domain._
 
 object Main extends App {
  import Holding._
- 
+ import Taxonomy._
+ import Filters._
+
  val g = Graph(
-   Investment("Portfolio")~>Investment("Equities") ### 0.5,
+   Investment("Global 60/40", Portfolio)~>Investment("Equities") ### 0.5,
      Investment("Equities")~>Investment("US Equities") ### 0.6,
+       Investment("US Equities")~>Investment("US Equities Excess Returns", ReturnStream) ### 1.0,
+       Investment("US Equities")~>Investment("USD Cash Returns", ReturnStream) ### 1.0,
      Investment("Equities")~>Investment("EUR Equities") ### 0.4,
+       Investment("EUR Equities")~>Investment("EUR Equities Excess Returns", ReturnStream) ### 1.0,
+       Investment("EUR Equities")~>Investment("EUR Cash Returns", ReturnStream) ### 1.0,
    Investment("Portfolio")~>Investment("Bonds") ### 0.5,
      Investment("Bonds")~>Investment("Nominal Bonds") ### 0.7,
-     Investment("Bonds")~>Investment("IL Bonds") ### 0.3)
-      
- g.nodes.foreach(n => println("Node = '" + n + "'"))
+       Investment("Nominal Bonds")~>Investment("Nominal Bonds Excess Returns", ReturnStream) ### 1.0,
+       Investment("Nominal Bonds")~>Investment("USD Cash Returns", ReturnStream) ### 1.0,
+     Investment("Bonds")~>Investment("IL Bonds") ### 0.3,
+       Investment("IL Bonds")~>Investment("IL Bonds Excess Returns", ReturnStream) ### 1.0,
+       Investment("IL Bonds")~>Investment("USD Cash Returns", ReturnStream) ### 1.0)
 
- val p = g get Investment("Portfolio")
+  // All the nodes
+  println(g.nodes mkString ":")
  
- val (n0, n1, n2) = (g get Investment("Portfolio"), g get Investment("Equities"), g get Investment("Bonds"))
- 
- val result = (ArrayBuffer.empty[String] /: p.innerNodeDownUpTraverser) {
-   (buf, param) => param match {
-     case (down, node) => 
-       if (down) buf += (if (node eq p) "(" else "[") += node.toString
-       else      buf += (if (node eq p) ")" else "]")
-   }
- }
- 
- println(result)
- 
-}
+  // All the holdings
+  println(g.edges mkString ":")
 
-case class Investment(name: String)
+  // The top level portfolio
+  val p = g get Investment("Global 60/40", Portfolio)
+ 
+  // All the nodes with a traverser 
+  println(p.outerNodeTraverser.map(_.toString()))
 
-case class User(name: String)
+  // Only the assets
+  println(p.outerNodeTraverser.filter(AssetsOnly).map(_.toString()))
+ 
+  // Only the return streams
+  println(p.outerNodeTraverser.filter(ReturnStreamsOnly).map(_.toString()))
 
-case class Holding[+N](portfolio: N, asset: N, amount: Double)
-  extends DiEdge[N](NodeProduct(portfolio, asset))
-  with    ExtendedKey[N]
-  with    EdgeCopy[Holding]
-  with    OuterEdge[N,Holding] 
-{
-  private def this(nodes: Product, amount: Double) {
-    this(nodes.productElement(0).asInstanceOf[N],
-         nodes.productElement(1).asInstanceOf[N], amount)
-  }
-  def keyAttributes = Seq(amount)
-  override def copy[NN](newNodes: Product) = new Holding[NN](newNodes, amount)
-  override protected def attributesToString = s" ($amount)" 
-}
-object Holding {
-  implicit final class ImplicitEdge[A <: Investment](val e: DiEdge[A]) extends AnyVal {
-    def ### (amount: Double) = new Holding[A](e.source, e.target, amount)
-  } 
+  // Same thing with a one way traverser
+  println((ArrayBuffer.empty[String] /: p.innerNodeTraverser)(_ += _.name).mkString)
+ 
+  // And with an up down traverser
+  println((ArrayBuffer.empty[String] /: p.innerNodeDownUpTraverser) {
+    (buf, param) => param match {
+      case (down, node) => 
+        if (down) buf += (if (node eq p) "(" else "[") += node.name
+        else      buf += (if (node eq p) ")" else "]")
+    }
+  }.mkString)
+ 
 }
